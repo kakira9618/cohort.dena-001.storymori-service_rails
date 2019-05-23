a82116e180a8000c490caf457e2483b038bbf978
Akira Kuroiwa <akira.kuroiwa@dena.com>
2019-05-23 14:00:51 +0900

    fix constraints

--- storymori/app/models/page.rb
+++ storymori/app/models/page.rb
@@ -1,4 +1,6 @@
 class Page < ApplicationRecord
   belongs_to :story
-  belongs_to :page, optional: true
+  
+  belongs_to :parent, class_name: "Page"
+  has_many :children, class_name: "Page", foreign_key: "parent_id"
 end
